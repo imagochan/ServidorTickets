a2564fdaefb3c9f4c7b32921a4ab42d185ce79cc
Julio <julioalfredomachado@gmail.com>
2023-08-03 00:12:20 -0600

    functional add product api from flutter

--- index.js
+++ index.js
@@ -8,6 +8,30 @@ app.use(express.urlencoded({
     extended:true
 }));
 
-app.listen(2000,()=>{
+const productData = [];
+
+app.listen(4185,()=>{
     console.log("Connected to server at 2000");
+})
+
+//post api
+
+app.post("/api/add_product",(req,res)=>{
+    console.log("Result",req.body);
+
+    const pdata = {
+        "id":productData.length+1,
+        "pname":req.body.pname,
+        "pprice":req.body.pprice,
+        "pdesc":req.body.pdesc
+    };
+    
+    productData.push(pdata);
+    console.log("Final",pdata);
+
+    res.status(200).send({
+        "status_code":200,
+        "message":"Product added succesfully",
+        "product":pdata
+    });
 })
