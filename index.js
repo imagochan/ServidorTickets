0b80afa760d55bda7e8888eeca2bc10881345bfb
Julio <julioalfredomachado@gmail.com>
2023-08-05 10:58:23 -0600

    testing adding data

--- index.js
+++ index.js
@@ -15,6 +15,15 @@ initializeApp({
 
 const db = getFirestore();
 
+// adding data
+const docRef = db.collection('users').doc('alovelace');
+
+await docRef.set({
+    first: 'Ada',
+    last: 'Lovelace',
+    born: 1815
+});
+
 app.use(express.json());
 
 app.use(express.urlencoded({
