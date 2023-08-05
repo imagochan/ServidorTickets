63463d5b3a3064a434ddac3a58646b79286a02d7
Julio <julioalfredomachado@gmail.com>
2023-08-05 10:57:31 -0600

    adding firestore initialization

--- index.js
+++ index.js
@@ -2,6 +2,19 @@ const express = require("express");
 
 const app = express();
 
+//initilize firestore on server
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+//const serviceAccount = require('./path/to/serviceAccountKey.json');
+const serviceAccount = require('json_key/test-project-fb8e5-90cf978b8a0b.json');
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
 app.use(express.json());
 
 app.use(express.urlencoded({
