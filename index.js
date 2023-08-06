15b6ab98a421a4e0c9035fdd2c85b6369916ff76
Julio <julioalfredomachado@gmail.com>
2023-08-06 00:38:27 -0600

    code cleaning

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const express = require("express");
-
 const app = express();
-
 const { db } = require('./firebase.js');
+const ticketCollectionRef = db.collection('tickets');
 
 app.use(express.json());
-
 app.use(express.urlencoded({
   extended: true
 }));
-
-const productData = [];
 
 var ticketData = [];
 
@@ -18,57 +14,7 @@ app.listen(2000, () => {
   console.log("Connected to server at 2000");
 })
 
-//post api
-
-app.post("/api/add_product", (req, res) => {
-  console.log("Result", req.body);
-
-  const pdata = {
-    "id": productData.length + 1,
-    "pname": req.body.pname,
-    "pprice": req.body.pprice,
-    "pdesc": req.body.pdesc
-  };
-
-  productData.push(pdata);
-  console.log("Final", pdata);
-
-  res.status(200).send({
-    "status_code": 200,
-    "message": "Product added succesfully",
-    "product": pdata
-  });
-})
-
-//post api ticket
-
-app.post("/api/add_ticket2", (req, res) => {
-  console.log("Result", req.body);
-
-  const tdata = {
-    "tid": ticketData.length + 1,
-    "ttitulo": req.body.ttitulo,
-    "tdescripcion": req.body.tdescripcion,
-    "tfechaVencimiento": req.body.tfechaVencimiento,
-    "tfechaPublicacion": req.body.tfechaPublicacion,
-    "tfechaFinPublicacion": req.body.tfechaFinPublicacion,
-    "tvalorCompra": req.body.tvalorCompra,
-    "tcategoria": req.body.tcategoria,
-  };
-
-  ticketData.push(tdata);
-  console.log("Final", tdata);
-
-  res.status(200).send({
-    "status_code": 200,
-    "message": "Ticket added succesfully",
-    "ticket": tdata
-  });
-})
-
-//post add api ticket into firebase
-const ticketCollectionRef = db.collection('tickets');
-
+//add ticket api with firestore
 app.post("/api/add_ticket", async (req, res) => {
 
   const res2 = await ticketCollectionRef.add({
@@ -84,26 +30,10 @@ app.post("/api/add_ticket", async (req, res) => {
   res.status(200).send({
     "status_code": 200,
     "message": "Ticket added succesfully",
-    //"ticket": tdata
   });
 })
 
-// get api
-app.get('/api/get_product', (req, res) => {
-  if (productData.length > 0) {
-    res.status(200).send({
-      'status_code': 200,
-      'products': productData
-    });
-  } else {
-    res.status(200).send({
-      'status_code': 200,
-      'products': []
-    });
-  }
-})
-
-// get ticket api
+// get ticket api with firestore
 app.get('/api/get_ticket', async (req, res) => {
 
   const snapshot = await ticketCollectionRef.get();
@@ -141,45 +71,8 @@ app.get('/api/get_ticket', async (req, res) => {
   }
 })
 
-//update api put
-
-app.post("/api/update/:id", (req, res) => {
-  let id = req.params.id * 1;//to return integer
-  let productToUpdate = productData.find(p => p.id === id);
-  let index = productData.indexOf(productToUpdate);
-
-  productData[index] = req.body;
-
-  res.status(200).send({
-    'status': "success",
-    'message': "Product updated"
-  })
-
-})
-
-//update ticket api post
-
-app.post("/api/update_ticket2/:id", (req, res) => {
-  let id = req.params.id * 1;//to return integer
-  let ticketToUpdate = ticketData.find(t => t.tid === id);//might need to chage id to tid if it doesn't work
-  let index = ticketData.indexOf(ticketToUpdate);
-
-  ticketData[index] = req.body;
-
-  res.status(200).send({
-    'status': "success",
-    'message': "Ticket updated"
-  })
-
-  console.log("Ticket data is ", ticketData);
-  //console.log("Tdata is ",tdata);
-
-})
-
-//update ticket api post with firebase!!!!!!!!!!
-
+//update ticket api with firestore
 app.post("/api/update_ticket/:id", async (req, res) => {
-
   const ticketDocumentRef = db.collection('tickets').doc(req.params.id)
 
   const res2 = await ticketDocumentRef.set({
@@ -192,53 +85,16 @@ app.post("/api/update_ticket/:id", async (req, res) => {
     categoria:req.body.categoria,
   })
 
-
   res.status(200).send({
     'status': "success",
     'message': "Ticket updated"
   })
-
 })
 
-
-// delete api
-
-app.post("/api/delete/:id", (req, res) => {
-  let id = req.params.id * 1;//to return integer
-  let productToUpdate = productData.find(p => p.id === id);
-  let index = productData.indexOf(productToUpdate);
-
-  productData.splice(index, 1);
-
-  res.status(204).send({
-    'status': "success",
-    'message': "Product deleted"
-  })
-})
-
-// delete ticket api
-
-app.post("/api/delete_ticket2/:id", (req, res) => {
-  let id = req.params.id * 1;//to return integer
-  let ticketToUpdate = ticketData.find(t => t.tid === id);
-  let index = ticketData.indexOf(ticketToUpdate);
-
-  ticketData.splice(index, 1);
-
-  res.status(204).send({
-    'status': "success",
-    'message': "Product deleted"
-  })
-})
-
-// delete ticket api with firebase
-
+// delete ticket api with firestore
 app.post("/api/delete_ticket/:id", async (req, res) => {
 
   const ticketDocumentRef = db.collection('tickets').doc(req.params.id).delete();
-
-  //const res2 = await ticketDocumentRef.delete
-
 
   res.status(200).send({
     'status': "success",
