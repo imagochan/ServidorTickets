f94713a9eb24cd0d1a3952dc805e6d665e6c6504
Julio <julioalfredomachado@gmail.com>
2023-08-06 01:23:11 -0600

    changed firebase json path and name

--- firebase.js
+++ firebase.js
@@ -3,7 +3,7 @@ const { initializeApp, applicationDefault, cert } = require('firebase-admin/app'
 const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
 
 //const serviceAccount = require('./path/to/serviceAccountKey.json');
-const serviceAccount = require('./json_key/test-project-fb8e5-90cf978b8a0b.json');
+const serviceAccount = require('./serviceAccountKey.json');
 
 initializeApp({
     credential: cert(serviceAccount)
